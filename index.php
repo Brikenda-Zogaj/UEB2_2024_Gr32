ffd2461412945041a9f87818389b024939c11204
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-04-08 21:47:07 +0200

    Added location-based redirection logic for property search form

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php 
 session_start();
 ?>
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $location=$_POST['searchInput'];
+    switch($location){
+        case 'Rio De Janeiro':
+            header("Location: properties.php#RioDeJaneiro");
+            exit();
+        case 'Tokyo':
+            header("Location: properties.php#Tokyo");
+            exit();
+        case 'London':
+            header("Location: properties.php#London");
+            exit();
+        default:
+            header("Location: properties.php"); // Default redirect if no location matches
+            exit();
+    }
+} 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,3 +58,35 @@ else
 $_SESSION['Visitors']=1;
 
  ?>
+ <!-- Featured Properties Section -->
+ <section class="featured-properties">
+ <h2>Featured Properties</h2>
+ <div class="property-grid">
+              <div class="property-item">
+                     <img src="./Assets/images/italy3.jpg" alt="Property 1">
+                     <div class="property-info">
+                         <h3>Elegant houses</h3>
+                         <p>Modern houses with a view of the beach.</p>
+                         <a href="#contact" class="btn btn-secondary">Contact Us</a>
+                     </div>
+                 </div>
+                 <div class="property-item">
+                     <img src="./Assets/images/marbella.jpg" alt="Property 1">
+                     <div class="property-info">
+                         <h3>Appartments</h3>
+                         <p>Discover urban living at its finest apartments</p>
+                         <a href="#contact" class="btn btn-secondary">Contact Us</a>
+                     </div>
+                 </div>
+                 <div class="property-item">
+                     <img src="./Assets/images/t4.jpeg" alt="Property 1">
+                     <div class="property-info">
+                         <h3>Village house</h3>
+                         <p>Excape to the countryside living in village houses</p>
+                         <a href="#contact" class="btn btn-secondary">Contact Us</a>
+                     </div>
+                 </div>
+        </div>
+ </section>
+</body>
+</html>
