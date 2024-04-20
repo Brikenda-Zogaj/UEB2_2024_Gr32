c83848b618a8a759f32ea5b89794314b37d3c6d3
gr3sa-h1 <154631164+gr3sa-h1@users.noreply.github.com>
2024-04-20 15:15:58 +0200

    Added Contact css

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   
-  <link href="../Css/contact.css" rel="stylesheet">
+  <link href="Css\contact.css" rel="stylesheet">
  
   
 </head>
