9674ab25d76e49cdfd030101b65b3a548d1a53d2
gr3sa-h1 <154631164+gr3sa-h1@users.noreply.github.com>
2024-05-28 15:00:32 +0200

    Krijimi i tabeles User dhe manipulimi me te dhena

--- db.php
+++ db.php
@@ -10,6 +10,50 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Kontrollimi i lidhjes
 if ($conn->connect_error) {
     die("Lidhja dështoi: " . $conn->connect_error);
-    echo "hc";
 }
+$sql = "CREATE TABLE IF NOT EXISTS  users (
+    
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    password VARCHAR(255) NOT NULL
+    
+    )";
+    
+    if ($conn->query($sql) === TRUE) {
+        //echo "Tabela Users u krijua me sukses";
+    } else {
+        echo "Gabim gjatë krijimit të tabelës: " . $conn->error;
+    }
+    $sql = "INSERT INTO users (first_name, last_name, username, email, password)
+    VALUES ('Syzana', 'Kryeziu', 'zannak', 'kryeziusyzana@gmail.com', 'zanna.123'),
+    ('Gresa', 'Halili', 'gresah', 'hresahalili@gmail.com', 'gresah.123'),('Brikenda', 'Zogaj', 'brikendaz', 'brikendazogaja@gmail.com', 'brikenda.123')
+   ,('Blerina', 'Balaj', 'blerinab', 'blerinabalaja@gmail.com', 'blerina123'),('Anna', 'Gashi', 'annag', 'annagashi@gmail.com', 'anna.123')" ;
+if ($conn->query($sql) === TRUE) {
+   
+} else {
+    echo "Gabim gjatë insertimit: " . $conn->error;
+}   
+
+   
+    
+    $sql = "UPDATE users SET email='syzana.kryeziu@gmail.com' WHERE first_name='Syzana'";
+    if ($conn->query($sql) === TRUE) {
+        
+    } else {
+        echo "Gabim gjatë update-imit: " . $conn->error;
+    }
+
+
+    $sql = "DELETE FROM users WHERE first_name='Anna'";
+    if ($conn->query($sql) === TRUE) {
+       
+    } else {
+        echo "Gabim gjatë fshirjes: " . $conn->error;
+    }
+     
+    $conn->close();
+
+?>
 
